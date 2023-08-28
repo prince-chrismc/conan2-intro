967496c3e6c43d7617a55f385fc5a01005c10c35
Christopher McArthur <christopherm@jfrog.com>
2023-08-28 14:12:02 -0700

    Basic Hello World project with spdlog and Conan

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # conan2-intro
+
 Sample Project for Presenting
+
+## Instructions
+
+1. `conan install`
+2. `cmake --preset conan-release`
+3. `cmake --build --preset conan-release`
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+// https://github.com/gabime/spdlog/blob/v1.x/README.md#usage-samples
+#include "spdlog/spdlog.h"
+
+int main() 
+{
+    spdlog::info("Welcome to spdlog!");
+    return 0;
+}
